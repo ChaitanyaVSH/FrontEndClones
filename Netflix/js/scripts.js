49c1da230e1093afaeda8fb4ba37c3004d45da43
Chaitanya <chaitu.vsh@gmail.com>
2021-02-07 23:58:30 +0530

    PRO JS to enable tab content

--- Netflix/js/scripts.js
+++ Netflix/js/scripts.js
@@ -1,1 +1,32 @@
-console.log("App is up and running.... Yay!")
+let tabsItems = document.querySelectorAll(".tab-item");
+let tabsContentItems = document.querySelectorAll(".tab-content-item");
+
+// Click handler
+function clicktoAction(event){
+
+    // Removes border for other tabs
+    removeBorder();
+    removeShow();
+
+    // Add border
+    this.classList.add("tab-border")
+
+    // Grab the  Tab content item from DOM
+
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    tabContentItem.classList.add("show")
+}
+
+function removeBorder(){
+    tabsItems.forEach(item => item.classList.remove("tab-border"))
+}
+
+function removeShow(){
+    tabsContentItems.forEach(item => item.classList.remove("show"))
+}
+
+
+// Listen for tab clicks
+tabsItems.forEach(tabItem => {
+    tabItem.addEventListener("click",clicktoAction)
+});
