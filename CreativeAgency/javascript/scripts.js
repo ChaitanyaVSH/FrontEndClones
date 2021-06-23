ce2b28812060dd32f69971526d6732817237c593
Chaitanya <chaitu.vsh@gmail.com>
2021-06-24 00:06:37 +0530

    Implemented Dark mode

--- CreativeAgency/javascript/scripts.js
+++ CreativeAgency/javascript/scripts.js
@@ -1,6 +1,11 @@
-let button = document.querySelector(".toggle")
+let button = document.querySelector(".toggle");
+let themeToggler = document.querySelector(".darkThemeToggler");
 
 button.addEventListener("click", () => {
     let navigationMenu = document.querySelector(".navigation")
     navigationMenu.classList.toggle("show")
-})
+});
+
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme")
+});
